932669fc475da3ae82b5e2806de1fcec916f780f
Jabba Schrillo <jabba@seschrillos.de>
2015-12-10 23:55:30 +0100

    reworked

--- JWOL/JWOL/Program.cs
+++ JWOL/JWOL/Program.cs
@@ -1,65 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Net;
-using System.Globalization;
+using System;
 
 namespace JWOL
 {
-    // we derive our class from a standart one
-    public class WOLClass : UdpClient
-    {
-        public WOLClass() : base()
-        { }
-        //this is needed to send broadcast packet
-        public void SetClientToBrodcastMode()
-        {
-            if (this.Active)
-                this.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 0);
-        }
-    }
-
-
     class Program
     {
-        //now use this class
-        //MAC_ADDRESS should  look like '013FA049'
-        private void WakeFunction(string MAC_ADDRESS)
+        /// <summary>
+        /// Converts an mac string to an byte array
+        /// </summary>
+        /// <param name="mac">The physical MAC address to convert. The string can have on of the fallowing formats.
+        /// <list type="bullet">
+        /// <item>
+        /// <description>AA:BB:CC:DD:EE:FF</description>
+        /// </item>
+        /// <item>
+        /// <description>AA-BB-CC-DD-EE-FF</description>
+        /// </item>
+        /// <item>
+        /// <description>AABB.CCDD.EEFF</description>
+        /// </item>
+        /// <item>
+        /// <description>AABBCCDDEEFF</description>
+        /// </item>
+        /// </list>
+        /// </param>
+        /// <exception cref="ArgumentNullException">thrown if <paramref name="mac"/> is null </exception>
+        /// <exception cref="ArgumentException">thrown if <paramref name="mac"/> is not valid</exception>
+        public static byte[] GetMacArray(string mac)
         {
-            WOLClass client = new WOLClass();
-            client.Connect(new
-               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
-                         0x2fff); // port=12287 let's use this one 
-            client.SetClientToBrodcastMode();
-            //set sending bites
-            int counter = 0;
-            //buffer to be send
-            byte[] bytes = new byte[1024];   // more than enough :-)
-                                             //first 6 bytes should be 0xFF
-            for (int y = 0; y < 6; y++)
-                bytes[counter++] = 0xFF;
-            //now repeate MAC 16 times
-            for (int y = 0; y < 16; y++)
+            if (string.IsNullOrEmpty(mac)) throw new ArgumentNullException("mac");
+            byte[] ret = new byte[6];
+            try
             {
-                int i = 0;
-                for (int z = 0; z < 6; z++)
+                string[] tmp = mac.Split(':', '-');
+                if (tmp.Length != 6)
                 {
-                    bytes[counter++] =
-                        byte.Parse(MAC_ADDRESS.Substring(i, 2),
-                        NumberStyles.HexNumber);
-                    i += 2;
+                    tmp = mac.Split('.');
+                    if (tmp.Length == 3)
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            ret[i * 2] = byte.Parse(tmp[i].Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+                            ret[i * 2 + 1] = byte.Parse(tmp[i].Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+                        }
+                    }
+                    else
+                        for (int i = 0; i < 12; i += 2)
+                            ret[i / 2] = byte.Parse(mac.Substring(i, 2), System.Globalization.NumberStyles.HexNumber);
                 }
+                else
+                    for (int i = 0; i < 6; i++)
+                        ret[i] = byte.Parse(tmp[i], System.Globalization.NumberStyles.HexNumber);
             }
-
-            //now send wake up packet
-            int reterned_value = client.Send(bytes, 1024);
+            catch
+            {
+                throw new ArgumentException("Argument doesn't have the correct format: " + mac, "mac");
+            }
+            return ret;
         }
+
+        /// <summary>
+        /// Sends a Wake-On-Lan packet to the specified MAC address.
+        /// </summary>
+        /// <param name="mac">Physical MAC address to send WOL packet to.</param>
+        private static void WakeOnLan(byte[] mac)
+        {
+            // WOL packet is sent over UDP 255.255.255.0:40000.
+            UdpClient client = new UdpClient();
+            client.Connect(IPAddress.Broadcast, 40000);
+
+            // WOL packet contains a 6-bytes trailer and 16 times a 6-bytes sequence containing the MAC address.
+            byte[] packet = new byte[17 * 6];
+
+            // Trailer of 6 times 0xFF.
+            for (int i = 0; i < 6; i++)
+                packet[i] = 0xFF;
+
+            // Body of magic packet contains 16 times the MAC address.
+            for (int i = 1; i <= 16; i++)
+                for (int j = 0; j < 6; j++)
+                    packet[i * 6 + j] = mac[j];
+
+            // Send WOL packet.
+            client.Send(packet, packet.Length);
+        }
+
 
         static void Main(string[] args)
         {
+            WakeOnLan(GetMacArray("E0-CB-4E-9B-02-69"));
         }
     }
 }
